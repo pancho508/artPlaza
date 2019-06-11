0d339af75ac9db4a9786f4a083a85654f008cad2
thinkpad <xoalxiyetl@gmail.com>
2019-06-11 11:22:18 -0500

    working

--- client/src/components/register_login/register.js
+++ client/src/components/register_login/register.js
@@ -1,122 +1,156 @@
 import React, { Component } from 'react';
 import FormField from '../utils/forms/formfield';
 import { update, generateData, isFormValid } from '../utils/forms/formactions';
+// import Dialog from '@material-ui/core/Dialog';
 
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions/user_actions';
+// import { registerUser } from '../../actions/user_actions';
 
 class Register extends Component {
 
-    state={
-        formError: false, 
-        formSuccess: '',
+    state = {
+        formError: false,
+        formSuccess:false,
         formdata:{
-            name:{
-                element: 'imput',
+            name: {
+                element: 'input',
                 value: '',
                 config:{
-                    name: 'name_imput',
+                    name: 'name_input',
                     type: 'text',
-                    palceholder: 'Enter your name'
+                    placeholder: 'Enter your name'
                 },
                 validation:{
                     required: true
                 },
-                valid: false, 
-                touched: false, 
-                validationMessage: ''
+                valid: false,
+                touched: false,
+                validationMessage:''
             },
-            lastname:{
-                element: 'imput',
+            lastname: {
+                element: 'input',
                 value: '',
                 config:{
-                    lastname: 'lastname',
+                    name: 'lastname_input',
                     type: 'text',
-                    palceholder: 'Enter your lastname'
+                    placeholder: 'Enter your lastname'
                 },
                 validation:{
                     required: true
                 },
-                valid: false, 
-                touched: false, 
-                validationMessage: ''
+                valid: false,
+                touched: false,
+                validationMessage:''
             },
-            email:{
+            email: {
                 element: 'input',
                 value: '',
                 config:{
                     name: 'email_input',
                     type: 'email',
-                    palceholder: 'Enter your Email'
+                    placeholder: 'Enter your email'
                 },
-                validation: {
-                    required: true, 
+                validation:{
+                    required: true,
                     email: true
                 },
-                valid: false, 
-                touched: false, 
-                validationMessage: ''
+                valid: false,
+                touched: false,
+                validationMessage:''
             },
             password: {
                 element: 'input',
                 value: '',
-                config: {
+                config:{
                     name: 'password_input',
                     type: 'password',
-                    palceholder: 'Enter your password'
+                    placeholder: 'Enter your password'
                 },
                 validation:{
                     required: true
-                }, 
-                valid: false, 
-                touched: false, 
-                validationMessage: ''
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
             },
             confirmPassword: {
                 element: 'input',
                 value: '',
-                config: {
+                config:{
                     name: 'confirm_password_input',
                     type: 'password',
-                    palceholder: 'Confirm your password'
+                    placeholder: 'Confirm your password'
                 },
                 validation:{
                     required: true,
                     confirm: 'password'
-                }, 
-                valid: false, 
-                touched: false, 
-                validationMessage: ''
+                },
+                valid: false,
+                touched: false,
+                validationMessage:''
             }
         }
     }
-    
 
-    submitForm = () =>{
-
+    updateForm = (element) => {
+        const newFormdata = update(element,this.state.formdata,'register');
+        this.setState({
+            formError: false,
+            formdata: newFormdata
+        })
     }
+
+    // submitForm= (event) =>{
+    //     event.preventDefault();
+        
+    //     let dataToSubmit = generateData(this.state.formdata,'register');
+    //     let formIsValid = isFormValid(this.state.formdata,'register')
+
+    //     if(formIsValid){
+    //         this.props.dispatch(registerUser(dataToSubmit))
+    //         .then(response =>{ 
+    //             if(response.payload.success){
+    //                 this.setState({
+    //                     formError: false,
+    //                     formSuccess: true
+    //                 });
+    //                 setTimeout(()=>{
+    //                     this.props.history.push('/register_login');
+    //                 },3000)
+    //             } else {
+    //                 this.setState({formError: true})
+    //             }
+    //         }).catch(e => {
+    //             this.setState({formError: true})
+    //         })
+    //     } else {
+    //         this.setState({
+    //             formError: true
+    //         })
+    //     }
+    // }
 
     render() {
         return (
-            <div className='page_wrapper'>
-                <div  className='container'>
-                    <div  className='register_login_container'>
-                        <div  className='left'>
-                            <form onSubmit={(event)=> this.submitForm(event)}>
-                                <h2>Personal Information</h2>
-                                <div classname="form_block_two">
+            <div className="page_wrapper">
+                <div className="container">
+                    <div className="register_login_container">
+                        <div className="left">
+                            <form onSubmit={(event)=>  this.submitForm(event)}>
+                            <h2>Personal information</h2>
+                                <div className="form_block_two">
                                     <div className="block">
-                                        <FormField 
+                                        <FormField
                                             id={'name'}
                                             formdata={this.state.formdata.name}
-                                            change={(element) => this.updateForm(element)}
+                                            change={(element)=> this.updateForm(element)}
                                         />
                                     </div>
                                     <div className="block">
-                                        <FormField 
+                                        <FormField
                                             id={'lastname'}
                                             formdata={this.state.formdata.lastname}
-                                            change={(element) => this.updateForm(element)}
+                                            change={(element)=> this.updateForm(element)}
                                         />
                                     </div>
                                 </div>
@@ -124,12 +158,51 @@ class Register extends Component {
                                     <FormField
                                         id={'email'}
                                         formdata={this.state.formdata.email}
-                                    />    
+                                        change={(element)=> this.updateForm(element)}
+                                    />
+                                </div>
+                                <h2>Verify password</h2>
+                                <div className="form_block_two">
+                                    <div className="block">
+                                        <FormField
+                                            id={'password'}
+                                            formdata={this.state.formdata.password}
+                                            change={(element)=> this.updateForm(element)}
+                                        />
+                                    </div>
+                                    <div className="block">
+                                        <FormField
+                                            id={'confirmPassword'}
+                                            formdata={this.state.formdata.confirmPassword}
+                                            change={(element)=> this.updateForm(element)}
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    { this.state.formError ?
+                                        <div className="error_label">
+                                            Please check your data
+                                        </div>
+                                    :null}
+                                    <button onClick={(event)=> this.submitForm(event)}>
+                                        Create an account
+                                    </button>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
+                </div>     
+
+                {/* <Dialog open={this.state.formSuccess}>
+                    <div className="dialog_alert">
+                        <div>Congratulations !!</div>
+                        <div>
+                            You will be redirected to the LOGIN in a couple seconds...
+                        </div>
+                    </div>
+                </Dialog> */}
+
+
             </div>
         );
     }
